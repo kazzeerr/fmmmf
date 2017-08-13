e8ea95a7ea7711cc5f693f3ac48e34c39e67849f
kazem1234 <kazem12000m@gmail.com>
2017-08-13 15:25:31 +0300

    Create KAZEM_Auto.sh

---
+++ KAZEM_Auto.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+COUNTER=1
+while(true) do
+./KAZEM.sh
+curl "https://api.telegram.org/bot[362136053:AAGkJVHSOaAHFADlrQmKDn28AGLDNIbEA5c]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#TshAkE-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
